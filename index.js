c5433bd983ddb75c4950a4f9eb8f164e39056e3d
ZawHtet-246098 <zhtet1636@gmail.com>
2023-08-12 08:14:57 +0630

    12-8-2023

--- index.js
+++ index.js
@@ -7,27 +7,31 @@ import userRoute from "./routes/user.js";
 import bookRoute from "./routes/books.js";
 
 const app = express();
+// hello
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send(`App is runnging`);
+});
+
+
 app.use("/user", userRoute);
-app.use("/", bookRoute);
+app.use("/books", bookRoute);
 
-// app.get("/", (req, res) => {
-//   res.send("App is running");
-// });
 
-const CONNECTION_URL =
+
+const CONNNECTION_URL =
   "mongodb+srv://ZawHtet:zawhtet150mongodb@cluster0.fpdqu.mongodb.net/Books?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(CONNNECTION_URL)
   .then(() =>
-    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+    app.listen(PORT, () => console.log(`Server is Running on port ${PORT}`))
   )
-  .catch((error) => console.log(`${error} did not connct`));
+  .catch((error) => console.log(`${error} did not connect`));
